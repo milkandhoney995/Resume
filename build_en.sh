118a37f036cc55c163b9b2e9ed9f40927f493e43
milkandhoney995 <harada.g17@gmail.com>
2026-01-13 11:38:42 +0900

    CV用テンプレート追加

--- build_en.sh
+++ build_en.sh
@@ -2,7 +2,7 @@
 set -e
 python3 generate.py en
 pandoc resume_en.md \
-  --reference-doc=template.docx \
+  --reference-doc=template_en.docx \
   --lua-filter=table_style.lua \
   --lua-filter=pagebreak_by_company.lua \
   -o CV.docx
